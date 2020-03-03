2f9b7122c357d42ca7dfd03eb0be71a35cc76ef9
Omar Gonzalez <ogonzaleznyc@gmail.com>
2020-03-03 05:19:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Write your code here!
 //document.querySelector('main').remove();
+document.getElementById('main').remove();
 // let newHeader = document.createElement("h1");
 // newHeader.id = 'victory';
 // newHeader.innerHTML = 'Omar is the champion';
