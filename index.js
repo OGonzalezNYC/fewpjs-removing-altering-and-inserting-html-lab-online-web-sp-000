5ed206268e53cc659c9e6fac7c0b5c259298b88b
Omar Gonzalez <ogonzaleznyc@gmail.com>
2020-03-03 05:17:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Write your code here!
 document.querySelector('main').remove();
+<<<<<<< HEAD
 let newHeader = document.createElement("h1");
 newHeader.id = 'victory';
 newHeader.innerHTML = 'Omar is the champion';
+=======
+let newHeader = document.getElementbyId('victory');
+>>>>>>> 7d23781fbadaf68c4a6166cb362064610316e260
